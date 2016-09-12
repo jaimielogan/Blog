f3a0a8129d3bcc02a15a52249097081d0ae8e9d9
jaimielogan <jaimielogan@gmail.com>
2016-09-12 11:44:32 -0600

    Unstyled forms completed for login, register, and post blog

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,28 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Breath of Fresh Air' });
 });
 
+router.get('/register', function(req,res,next){
+  res.render('register');
+});
+
+router.post('/register', function(req,res,next){
+  console.log("Post Reached");
+});
+
+router.get('/login', function(req,res,next){
+  res.render('login');
+});
+
+router.post('/login', function(req,res,next){
+  console.log("Login Reached");
+});
+
+router.get('/post', function(req,res,next){
+  res.render('post');
+});
+
+router.post('/post', function(req,res,next){
+  console.log("Blog Post Reached");
+});
+
 module.exports = router;
