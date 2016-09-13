34b441e38b581fc096e1a2e9964641a8483165a7
jaimielogan <jaimielogan@gmail.com>
2016-09-13 16:37:50 -0600

    More updates for production deployment

--- db/knex.js
+++ db/knex.js
@@ -1,4 +1,5 @@
 var environment = process.env.NODE_ENV || 'development';
+console.log('environment in knex.js',environment);
 var config = require('../knexfile.js')[environment];
 
 module.exports = require('knex')(config);
